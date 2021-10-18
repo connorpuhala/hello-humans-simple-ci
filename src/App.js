6bb15da079626f5cbc251358b7a5ffbe600e1d0d
Connor <connorpuhala@gmail.com>
2021-10-18 12:42:54 -0400

    my guy

--- src/App.js
+++ src/App.js
@@ -15,7 +15,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          Learn React My Guy!
         </a>
       </header>
     </div>
