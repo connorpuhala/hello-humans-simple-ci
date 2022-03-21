6e1352e84e10661d6dee2f1951e0c266b9129eec
Connor <connorpuhala@gmail.com>
2022-03-20 21:36:11 -0700

    adding links

--- src/App.js
+++ src/App.js
@@ -6,6 +6,16 @@ import BackgroundText from './background.js';
 function App() {
   return (
     <div className="App">
+      <div class="dropdown show">
+        <button id="dropdownMenuLink" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+          :)
+        </button>
+        <div class="dropdown-menu" aria-labelledby="dropdownMenuLink">
+          <a class="dropdown-item" href="https://docs.google.com/document/d/e/2PACX-1vSTnFMoJZRLJCzGb8YoOe860dEBjiYTUwDY02WaVVBl0udR-nkVZqLD4l1gB2fbbxssktxX7xxU6jVL/pub" target="_blank">📝 Whitepaper</a>
+          <a class="dropdown-item" href="https://docs.google.com/document/d/e/2PACX-1vQUnWZoxr-euOFQEVTPFlLvpLV_IGQDXJG9b4CJaepPnb0ndlDrMQiXcE_8eIAmt9AjpGyKTcr9-Nj-/pub" target="_blank">📕 Full PDF</a>
+          <a class="dropdown-item" href="https://twitter.com/HelloHumansAI" target="_blank">🕊️ Twitter</a>          
+        </div>
+      </div>
       <header className="App-header">
         <BackgroundText />
         <div className="help-icon-container">
@@ -23,7 +33,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          <img src={cover} alt="cover" className="cover"/>
+          <img src={cover} alt="cover" className="cover" />
           <p>$11</p>
         </a>
       </header>
